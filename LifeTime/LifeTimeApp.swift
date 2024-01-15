f17728eceb6811416c3fe40f8a0736e23aba9b62
dmi-uni <danilmsnv@gmail.com>
2024-01-15 12:20:05 +0500

    Firebase setup

--- LifeTime/LifeTimeApp.swift
+++ LifeTime/LifeTimeApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import FirebaseCore
 
 @main
 struct LifeTimeApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
